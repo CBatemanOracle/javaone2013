abd3512f033b2e1a3cec84622d8d4d2727c57db6
Cameron Bateman <cameron.bateman@oracle.com>
2013-09-22 21:21:14 -0700

    Refactor a few ui blocks.

--- CrudApp1/WebContent/scripts/message_panel.js
+++ CrudApp1/WebContent/scripts/message_panel.js
@@ -1,70 +1,46 @@
 g_lastSaved = -1;
 
-
-
-function MessagePanel(message)
+function ListItem(message)
 {
 	this.message = message;
+
 	this.createContents = function(parentId)
 	{
-		var messageId = this.message.createId("li");
+		this.elementId = this.message.createId("li");
+		elementById(parentId).append(createElement("li", {id : this.elementId, tweetid : this.message.tweetId}));
+	};
+}
+function EditableLabel(message)
+{
+	this.message = message;
+	
+	this.createContents = function(parentId)
+	{
+		this.elementId = this.message.createId("span");
+		elementById(parentId).append(createElement("span", {id:this.elementId}, this.message.text));
+		
+		this.inputId = this.message.createId("input");
+		elementById(parentId).append(createElement("input", {id:this.inputId, style:"display: none;"}));
+		var outer = this;
 
-		elementById(parentId).append(createElement("li", {id :messageId, tweetid : this.message.tweetId}));
-		elementById(messageId).append(createElement("span", {id:this.message.createId("span")}, this.message.text));
-		var spanId = this.message.createId("span");
-		elementById(spanId).click(
+		elementById(this.elementId).click(
 			function() {
-				elementById(spanId).css('display', 'none');
-				elementById(inputId).val(elementById(spanId).text()).css('display',
+				elementById(outer.elementId).css('display', 'none');
+				elementById(outer.inputId).val(elementById(outer.elementId).text()).css('display',
 						'').focus();
 			});
-		var inputId = this.message.createId("input");
-		elementById(messageId).append(createElement("input", {id:inputId, style:"display: none;"}));
-		var outer = this;
-		elementById(inputId).blur(function(){outer.handleEdit(message);});
-        elementById(inputId).keydown(function(event){
+		elementById(this.inputId).blur(function(){outer.handleEdit(message);});
+        elementById(this.inputId).keydown(function(event){
             if (event.which == 13)
             {
                 outer.handleEdit(message);
             }
         });
-        var successIconId = this.message.createId("successIconSpan");
-        var successIconDisplayId = this.message.id == g_lastSaved ? "display: '';" : "display: none;";
-
-        elementById(messageId).append(createElement("span", {id:successIconId, style:successIconDisplayId}));
-        elementById(successIconId).append(createElement("img", {alt:"Saved", src:"icons/success.png"}));
-        
-		if (this.message.tweetId == -1) {
-		    var removeId = this.message.createId("removeIcon");
-			elementById(messageId).append(createElement("span", {
-				id : removeId,
-				onclick : "onDelete(" + this.message.id + ")"
-			}));
-			elementById(removeId).append(createElement("img", {
-			    alt : "Delete",
-			    src : "icons/remove.png"}));
-			
-			var tweetId = this.message.createId("tweetIcon");
-			elementById(messageId).append(createElement("span",{
-			   id : tweetId}));
-			elementById(tweetId).click(function(){
-			   onTweet(this.message);
-			});
-			elementById(tweetId).append(createElement("img", {
-			   alt : "Tweet", src:"https://twitter.com/images/b2_btn_icon.gif"}));
-		}
-
-		elementById(messageId).append(createElement("div", {
-					style : "font-size: 50%;"
-				}, "Last modified: " + formatDate(this.message.lastsave)));
 	};
 	
-
-
 	this.handleEdit = function(message) {
-		var inputId = message.createId("input");
-		elementById(inputId).css('display', 'none');
-		var newText = elementById(inputId).val();
+		elementById(this.inputId).css('display', 'none');
+		var newText = elementById(this.inputId).val();
 		var spanId = message.createId("span");
 		elementById(spanId).text(newText).css('display', '');
 		message.text = newText;
@@ -73,6 +49,63 @@ function MessagePanel(message)
 			readMessages(onRefreshSuccess);
 		});
 	};
+}
+
+function SuccessIcon(message)
+{
+	this.message = message;
+	
+	this.createContents = function(parentId)
+	{
+        var successIconId = this.message.createId("successIconSpan");
+        var successIconDisplayId = this.message.id == g_lastSaved ? "display: '';" : "display: none;";
+
+        elementById(parentId).append(createElement("span", {id:successIconId, style:successIconDisplayId}));
+        elementById(successIconId).append(createElement("img", {alt:"Saved", src:"icons/success.png"}));
+	};
+	
+}
+
+function MessagePanel(message)
+{
+	this.message = message;
+	this.createContents = function(parentId)
+	{
+		var listItem = new ListItem(this.message);
+		listItem.createContents(parentId);
+
+		var editableLabel = new EditableLabel(message);
+		editableLabel.createContents(listItem.elementId);
+
+		var successIcon = new SuccessIcon(message);
+		successIcon.createContents(listItem.elementId);
+		
+        
+		if (this.message.tweetId == -1) {
+		    var removeId = this.message.createId("removeIcon");
+			elementById(listItem.elementId).append(createElement("span", {
+				id : removeId,
+				onclick : "onDelete(" + this.message.id + ")"
+			}));
+			elementById(removeId).append(createElement("img", {
+			    alt : "Delete",
+			    src : "icons/remove.png"}));
+			
+			var tweetId = this.message.createId("tweetIcon");
+			elementById(listItem.elementId).append(createElement("span",{
+			   id : tweetId}));
+			elementById(tweetId).click(function(){
+			   onTweet(this.message);
+			});
+			elementById(tweetId).append(createElement("img", {
+			   alt : "Tweet", src:"https://twitter.com/images/b2_btn_icon.gif"}));
+		}
+
+		elementById(listItem.elementId).append(createElement("div", {
+					style : "font-size: 50%;"
+				}, "Last modified: " + formatDate(this.message.lastsave)));
+	};
+	
 }
 
 function elementById(id)
